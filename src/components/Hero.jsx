366583ea5b8db532d7e9277a47dee8e70c221511
oliverhees <oliverhees@gmail.com>
2023-09-02 21:15:46 +0200

    add logo and navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center flex-col">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+      </nav>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
